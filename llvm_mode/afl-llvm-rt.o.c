578e5072629c543c144c972908e2bfa0e7a44fb1
lockone <ahcheong.lee@gmail.com>
2021-02-18 01:57:50 +0900

    fix

--- llvm_mode/afl-llvm-rt.o.c
+++ llvm_mode/afl-llvm-rt.o.c
@@ -1085,8 +1085,8 @@ void __func_log_hook(uint32_t cmpid, uint32_t condition) {
   if (unlikely(!__afl_func_map)) return;
 
   condition = condition ? 2 : 1;
-  __afl_func_map->entries[k].executed = 1; //mark executed
-  __afl_func_map->entries[k].condition |= condition;
+  __afl_func_map->entries[cmpid].executed = 1; //mark executed
+  __afl_func_map->entries[cmpid].condition |= condition;
 }
 
 ///// CmpLog instrumentation
